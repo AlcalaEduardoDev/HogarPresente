bc0a0b63f2fa90f72530ff6824184d6d5cd570aa
Eduardo-desing <alcalaeduardo6@gmail.com>
2020-11-17 14:56:39 -0300

    ya se muestran los cursos (api mock)

--- src/app/Service/lista-cursos.service.ts
+++ src/app/Service/lista-cursos.service.ts
@@ -9,7 +9,7 @@ import { Curso } from '../Interface/curso';
 export class ListaCursosService {
 
   cursoActivo : Curso;
-  apiBaseUrl : string = 'https://hogar-presente.herokuapp.com';
+  apiBaseUrl : string = 'https://e6b4b286-1a3f-4b6d-8663-7a6cb9f3fa22.mock.pstmn.io';
   constructor(private http: HttpClient) { }
   public findAll():Observable<any>{
     return this.http.get(this.apiBaseUrl + '/api/cursos');
